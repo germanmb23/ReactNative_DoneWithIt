9f93b0cfeea83828ee01393673b1b39457aa7bd3
German M <german.moreira@fing.edu.uy>
2021-04-03 08:03:22 -0300

    flex, wrap, align, justify

--- App.js
+++ App.js
@@ -1,15 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Button,
+  Alert,
+} from 'react-native';
 
 export default function App() {
   let x = 1;
   console.log('App executed');
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!aaaaa</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        backgroundColor: '#fff',
+        flex: 1,
+        flexDirection: 'row', // horizontal flex (I can set it vertically)
+        justifyContent: 'center', // x axis (or primary)
+        alignItems: 'center', // y axis (or secondary)
+        alignContent: 'center', //only works if we have wrapping
+        flexWrap: 'wrap',
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          width: 90,
+          height: 300,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'gold',
+          width: 90,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'tomato',
+          width: 90,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'grey',
+          width: 90,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'greenyellow',
+          width: 100,
+          height: 100,
+        }}
+      />
     </View>
   );
 }
