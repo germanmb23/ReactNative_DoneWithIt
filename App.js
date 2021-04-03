bd43e6182491c80031be90e8b01f80d6e9759fac
German M <german.moreira@fing.edu.uy>
2021-04-03 18:03:33 -0300

    Shadows

--- App.js
+++ App.js
@@ -11,70 +11,26 @@ import {
 } from 'react-native';
 
 export default function App() {
-  let x = 1;
-  console.log('App executed');
-
   return (
     <View
       style={{
-        backgroundColor: '#fff',
         flex: 1,
-        flexDirection: 'row', // horizontal flex (I can set it vertically)
-        justifyContent: 'center', // x axis (or primary)
-        alignItems: 'center', // y axis (or secondary)
-        alignContent: 'center', //only works if we have wrapping
-        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <View
         style={{
           backgroundColor: 'dodgerblue',
-          width: 90,
-          height: 300,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: 'gold',
-          width: 90,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: 'tomato',
-          width: 90,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: 'grey',
-          width: 90,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: 'greenyellow',
           width: 100,
           height: 100,
+          shadowColor: 'grey',
+          shadowoffset: { width: 0, height: 10 },
+          shadowOpacity: 1,
+          shadowRadius: 10,
+          elevation: 26,
         }}
       />
     </View>
   );
 }
-
-// View it's like a div
-// View -> UIView
-// View -> AndroidView
-// Map component into native widget
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
