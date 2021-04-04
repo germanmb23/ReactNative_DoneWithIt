66c6ef8869a58d317fd7285f5c2a168f9d7ed024
German M <german.moreira@fing.edu.uy>
2021-04-03 22:45:48 -0300

    Building The Button Component

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View } from 'react-native';
-import AppText from './app/components/AppText';
+import { StyleSheet, Text, View } from 'react-native';
+import AppButton from './app/components/AppButton';
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <AppText>I love React Native!</AppText>
-    </View>
-  );
+    <View style={{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+    }}>
+      <AppButton title='Login' onPress={()=> console.log('Tapped')}/>
+    </View>)
 }
