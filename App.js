cd83d8bf55290966777f00ec81f898b9309002e5
German M <german.moreira@fing.edu.uy>
2021-04-04 22:15:21 -0300

    Improving the View Image Screen

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { Text, View } from 'react-native';
 import Card from './app/components/Card';
 import ListItem from './app/components/ListItem';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
