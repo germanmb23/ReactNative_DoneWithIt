bafa8df1ec3eafc5c60c9daa148a1f877c3576e6
German M <german.moreira@fing.edu.uy>
2021-04-05 01:30:11 -0300

    Separators

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 //import Constants from 'expo-constants';
 
 import ListItem from '../components/ListItem';
+import ListItemSeparator from '../components/ListItemSeparator';
 import Screen from '../components/Screen';
 
 const messages = [
@@ -33,6 +34,7 @@ function MessagesScreen(props) {
             image={item.image}
           />
         )}
+        ItemSeparatorComponent={ListItemSeparator}
       />
     </Screen>
   );
