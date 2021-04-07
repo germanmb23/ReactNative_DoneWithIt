26f6a1df0ffd674b2c2e2f86cf9873948b4ec305
German M <german.moreira@fing.edu.uy>
2021-04-06 23:07:55 -0300

    Extracting the Default Styles

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import { Text, Platform, StyleSheet } from 'react-native';
 
-function AppText({ children, style }) {
-  return <Text style={[styles.text, style]}>{children}</Text>;
-}
+import defaultStyles from '../config/styles';
 
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 18,
-    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
-    color: 'black',
-  },
-});
+function AppText({ children, style }) {
+  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
+}
 
 export default AppText;
