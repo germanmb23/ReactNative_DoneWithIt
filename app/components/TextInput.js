1f6a80df79570fc96a7ea7530002082bd93730f3
German M <german.moreira@fing.edu.uy>
2021-04-11 04:59:28 -0300

    Custom Snippets

--- app/components/TextInput.js
+++ app/components/TextInput.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextInput, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import defaultStyles from '../config/styles';
