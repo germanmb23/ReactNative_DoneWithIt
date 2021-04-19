9391728837b74bdc3ca68b04ba488d5157950d48
German M <german.moreira@fing.edu.uy>
2021-04-19 02:28:34 -0300

    Fetching Data

--- app/screens/ListingScreen.js
+++ app/screens/ListingScreen.js
@@ -5,23 +5,22 @@ import Card from '../components/Card';
 import Screen from '../components/Screen';
 import colors from '../config/colors';
 import routes from '../navigation/routes';
-
-const listings = [
-  {
-    id: 1,
-    title: 'Red jacket for sale',
-    price: 100,
-    image: require('../assets/jacket.jpg'),
-  },
-  {
-    id: 2,
-    title: 'Couch in great condition',
-    price: 1000,
-    image: require('../assets/couch.jpg'),
-  },
-];
+import listingsApi from '../api/listings';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function ListingScreen({ navigation }) {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  const loadListings = async () => {
+    const response = await listingsApi.getListings();
+    setListings(response.data);
+  };
+
   return (
     <Screen style={styles.screen}>
       <FlatList
@@ -31,7 +30,7 @@ function ListingScreen({ navigation }) {
           <Card
             title={item.title}
             subTitle={'$' + item.price}
-            image={item.image}
+            imageUrl={item.images[0].url}
             onPress={() =>
               navigation.navigate(routes.LISTING_DETAILS, item)
             }
