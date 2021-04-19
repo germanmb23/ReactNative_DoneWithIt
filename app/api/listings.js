8f977469e4f10753b0b69599a4272c0523d3f40e
German M <german.moreira@fing.edu.uy>
2021-04-19 19:50:42 -0300

    Extending the API Layer

--- app/api/listings.js
+++ app/api/listings.js
@@ -4,6 +4,27 @@ const endpoint = '/listings';
 
 const getListings = () => client.get(endpoint);
 
+const addListing = (listings) => {
+  const data = new FormData();
+  data.append('title', listings.title);
+  data.append('price', listings.price);
+  data.append('categoryId', listings.category.value);
+  data.append('description', listings.description.value);
+
+  listings.images.forEach((image, index) =>
+    data.append('images', {
+      name: 'image' + index,
+      type: 'image/jpeg',
+      uri: image,
+    }),
+  );
+  if (listings.location)
+    data.append('location', JSON.stringify(listings.location));
+
+  return client.post(endpoint, data);
+};
+
 export default {
   getListings,
+  addListing,
 };
